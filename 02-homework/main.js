71bf3561760b066a249e74f7ac36dde3fd363bee
Artur P <artur.petruk@rambler.ru>
2022-05-26 20:35:25 +0300

    fixed code styling,corrected cycles,update variable

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -3,19 +3,18 @@ let M;
 
 
 while (!(N % 1 === 0 && Number.isInteger(parseInt(N)))) {
-    N = prompt('Enter the first number: ', '');
+    N = +prompt('Enter the first number: ', '');
     alert('Keep using INTEGERS only');
 }
-while (!(M % 1 === 0 && Number.isInteger(parseInt(M)) && +M > +N)) {
-    M = prompt('Enter the second number: ', '');
-    if (+M > +N) {
+while (!(M % 1 === 0 && Number.isInteger(parseInt(M)) && M > N)) {
+    M = +prompt('Enter the second number: ', '');
+    if (M > N) {
         alert('Next step, count even/odd numbers')
     } else {
         alert('Second number must be greater than first and not equal!')
     }
 }
-N = +N;
-M = +M;
+
 const skipEvenOdd = confirm('Skip even numbers?');
 
 
