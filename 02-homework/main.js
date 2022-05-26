56e70dbb037128226ff1d8cc9d301aec2835e141
Artur P <artur.petruk@rambler.ru>
2022-05-26 15:35:31 +0300

    fixed code styling,corrected cycles,update variable

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -1,32 +1,22 @@
 let N;
 let M;
-do {
-    N = prompt('Enter the first number: ', '');
-    if (N % 1 === 0 && Number.isInteger(parseInt(N))) {
-        alert('nice,now go to the second number');
-    } else {
-        alert('try again(INTEGERS only)');
-    }
-}
+
 
 while (!(N % 1 === 0 && Number.isInteger(parseInt(N)))) {
-    alert('keep using INTEGERS only');
+    N = prompt('Enter the first number: ', '');
+    alert('Keep using INTEGERS only');
 }
 
 do {
     M = prompt('Enter the second number: ', '');
-    if (M % 1 === 0 && Number.isInteger(parseInt(N))) {
-        alert('next step will count even/odds numbers');
-    } else {
-        alert('try again(INTEGERS only)');
-    }
-} while (!(M % 1 === 0 && Number.isInteger(parseInt(M)))) {
 
+} while (!(M % 1 === 0 && Number.isInteger(parseInt(M)) && M > N)) {
+    alert('Now we are going to count even/odd numbers')
 }
-
-const skipEvenOdd = confirm('Skip even numbers?');
 N = +N;
 M = +M;
+const skipEvenOdd = confirm('Skip even numbers?');
+
 
 let sum = 0;
 
@@ -38,4 +28,4 @@ for (let i = N; i <= M; i++) {
         console.log(sum);
     }
 }
-document.writeln(`result: ${sum}`);
+document.writeln(`Result: ${sum}`);
