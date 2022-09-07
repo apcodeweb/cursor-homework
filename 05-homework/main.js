21dbb403b1b33afa0cfcc606b18175c248b76687
Artur <artur.petruk22@gmail.com>
2022-09-07 15:40:08 +0300

    fixed code styling,methods

--- 05-homework/main.js
+++ 05-homework/main.js
@@ -6,6 +6,11 @@ const getRandomNumber = (length, min, max) => {
     return randomArray
 }
 console.log(getRandomNumber(15, 1, 100));
+document.writeln(`GetRandomNumber(15, 1, 100) = ${getRandomNumber(15,1,10)}<br><br>`)
+
+
+
+
 
 const getAverage = (...numbers) => {
     let newArr = numbers.filter(function(el) {
@@ -15,11 +20,22 @@ const getAverage = (...numbers) => {
     return average / newArr.length;
 }
 console.log(getAverage(1, 4, 6, 7, 8, 1.5, 9, 0, 19));
+document.writeln(`Average number of this set (1, 4, 6, 7, 8, 1.5, 9, 0, 19) = ${getAverage(1, 4, 6, 7, 8, 1.5, 9, 0, 19)}<br><br>`)
+
+
+
+
+
 
 const filterEvenNumbers = (...numbers) => {
     return numbers.filter(el => el % 2 !== 0);
 }
 console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6, 7));
+document.writeln(`Skip even numbers in this set of numbers(1,2,3,4,5,6,7) = ${filterEvenNumbers(1, 2, 3, 4, 5, 6,7)} <br><br>`)
+
+
+
+
 
 const countPositiveNumbers = (...numbers) => {
     let positiveNumb = numbers.filter(function(el) {
@@ -29,11 +45,24 @@ const countPositiveNumbers = (...numbers) => {
     return summary;
 }
 console.log(countPositiveNumbers(-1, 0, -24, 1, 2, 3, 4, 5, 6));
+document.writeln(`Sum of all numbers which > 0 (-1, 0, -24, 1, 2, 3, 4, 5, 6) =${countPositiveNumbers(-1, 0, -24, 1, 2, 3, 4, 5, 6)}<br> <br>`)
+
+
+
+
+
 
 const getDividedByFive = (...numbers) => {
     return numbers.filter(el => el % 5 === 0 && el !== 0);
 }
 console.log(getDividedByFive(-1, -5, 0, -24, 1, 5, 10, 20))
+document.writeln(`Get Divided By Five (-1, -5, 0, -24) =  ${getDividedByFive(-1, -5, 0, -24)}<br><br>`)
+
+
+
+
+
+
 
 const replaceBadWords = (string) => {
     const badWords = ["fuck", "shit"];
@@ -48,3 +77,4 @@ const replaceBadWords = (string) => {
     return newArr.join(' ');
 }
 console.log(replaceBadWords('fucking shit'));
+document.writeln(`Bad words marked by * = ${replaceBadWords('fucking shit')}`);
